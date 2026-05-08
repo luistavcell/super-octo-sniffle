921af0bf253af6e21c0bac0ae67e3cdd74d9d816
luis tavera <159097793+luistavcell@users.noreply.github.com>
2026-05-07 21:02:05 -0500

    Add files via upload

---
+++ FRUBER.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'fruver-fresh-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
